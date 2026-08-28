eee27aaecf56e05c3e1ef4d948f6ef6e0d49d729
caojj08267 <cjjwzs@gmail.com>
2026-08-28 21:15:06 +0800

    初始化项目

--- src/interfaceexample/MyInterface.java
+++ src/interfaceexample/MyInterface.java
@@ -8,12 +8,37 @@ public interface MyInterface {
         System.out.println(this.myName+"  "+this.age);
     }
 
+    static void staticPrint(){
+        System.out.println("staticPrint");
+    }
+
+    void myMethod();
     public static void main(String[] args) {
         new MyInterface() {
             @Override
             public void print() {
                 MyInterface.super.print();
             }
+
+            @Override
+            public void myMethod() {
+
+            }
         }.print();
+    }
+}
+
+class MyInerfaceImpl implements MyInterface {
+
+    @Override
+    public void myMethod() {
+        System.out.println("myMethod");
+    }
+
+    public static void main(String[] args) {
+        MyInterface myInterface = new MyInerfaceImpl();
+        myInterface.myMethod();
+        //myInterface.staticPrint();//Static method may be invoked on containing interface class only
+        myInterface.print();
     }
 }
